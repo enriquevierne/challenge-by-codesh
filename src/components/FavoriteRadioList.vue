d0f90181af891f1d3ffecb6564538c1df7e83858
Enrique Vierne <viernee@gmail.com>
2025-02-16 20:49:36 -0300

    feat: add radio station components with search functionality

---
+++ src/components/FavoriteRadioList.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="w-1/3 max-w- m-auto bg-gray-900 min-h-screen"></div>
+</template>
---
+++ src/components/Layout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="w-full min-h-screen bg-neutral-800">
+        <div class="m-auto">
+            <slot />
+        </div>
+    </div>
+</template>
